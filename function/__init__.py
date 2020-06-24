a309e799b816619e8fcc6fed5b541ec179b4ba35
raivolalala <lallo.raivo@gmail.com>
2020-06-24 23:02:22 +0300

    added the RIGHT FUCKING function code

--- function/__init__.py
+++ function/__init__.py
@@ -1,13 +1,27 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
+import pyodbc
 import urllib.request
 import json
 
+# tracking part
 req = urllib.request.Request("http://api.open-notify.org/iss-now.json")
 response = urllib.request.urlopen(req)
 
 obj = json.loads(response.read())
+timestamp = obj['timestamp']
+latitude = obj['iss_position']['latitude']
+longitude = obj['iss_position']['longitude']
 
-print(obj['timestamp'])
-# print(obj['iss_position']['latitude'], obj['data']['iss_position']['latitude'])
-print(obj['iss_position']['latitude'], obj['iss_position']['longitude'])
+# connection part
+server = 'raivoclout.database.windows.net'
+database = 'fromPS'
+username = 'admraivo'
+password = '$dmR4!v0'
+driver= '{ODBC Driver 17 for SQL Server}'
+cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+
+# update db
+cursor = cnxn.cursor()
+cursor.execute("INSERT INTO dbo.f_iss_coordinates VALUES(?, ?, ?);", (timestamp, latitude, longitude))
+cnxn.commit()
